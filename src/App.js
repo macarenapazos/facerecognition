4bb34ef6e06b8f1d41f5d888a49fd43d3d12bc04
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-12-14 16:29:24 -0300

    agregue el efecto particulas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
-import Navigation from './components/Navigation/Navigation.js';
-import Logo from './components/Logo/Logo.js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOpcions = {
+    particles: {
+    	number:{
+    		value: 250,
+    		density:{
+    			enable: true,
+    			value_area: 800
+    		}
+    	}
+    }       
+}
 
 function App() {
   return (
     <div className="App">
+
+        <Particles className='particles'
+            params={particlesOpcions}
+        />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
