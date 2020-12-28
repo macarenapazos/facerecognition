ebef94189962a450da540081b542cc2767c3d1c7
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-12-28 14:55:26 -0300

    Updates

---
+++ precache-manifest.1cbed0981b9748da170a9c8acde4be1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011490a30ab15d32878ac8912ebebd10",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "e216711e37d7083be79f",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e7aa7153f46e919b1673",
+    "url": "/facerecognition/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "e216711e37d7083be79f",
+    "url": "/facerecognition/static/js/2.0af95d72.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.0af95d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7aa7153f46e919b1673",
+    "url": "/facerecognition/static/js/main.9c98c60d.chunk.js"
+  },
+  {
+    "revision": "e457364e089452e68aa4",
+    "url": "/facerecognition/static/js/runtime-main.b0d2466f.js"
+  }
+]);
